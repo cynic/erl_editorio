f2706b4666fb0f6dfe3a35f40bfed5d261d78105
C <cinyc.s@gmail.com>
2023-07-30 20:09:59 +0200

    Added application behaviour

--- src/erl_editorio.erl
+++ src/erl_editorio.erl
@@ -1,9 +1,17 @@
 -module(erl_editorio).
+-behaviour(application).
+-export([start/2, stop/1]).
 -export([getch/0, clear/0]).
 -on_load(init/0).
 
 init() -> 
     erlang:load_nif("./erl_editorio", 0).
+
+start(_Type, _Args) ->
+    ok.
+
+stop(_State) ->
+    ok.
 
 getch() -> 
     erlang:nif_error(nif_not_loaded).
